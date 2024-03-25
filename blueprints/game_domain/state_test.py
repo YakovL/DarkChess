9f110f53c3d3d61ff333fe4cd60740e12402050d
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-03-25 17:16:37 +0300

    fix: test white king is in place, transpose initial board positions, test also one empty position

--- blueprints/game_domain/state_test.py
+++ blueprints/game_domain/state_test.py
@@ -3,18 +3,26 @@ from state import State, Player, Piece
 def test_init():
     """
     With default positions, nobody wins;
-    check also the corner pieces
+    check also the corner pieces and another two positions
     """
     game_state = State()
     assert game_state.get_winner() is None
 
-    left_bottom_piece = game_state.get_board().cells[0][0]
-    assert left_bottom_piece is not None
-    assert left_bottom_piece.piece == Piece.rook
-    assert left_bottom_piece.player == Player.white
+    bottom_left_piece = game_state.get_board().cells[0][0]
+    assert bottom_left_piece is not None
+    assert bottom_left_piece.piece == Piece.rook
+    assert bottom_left_piece.player == Player.white
 
     top_right_piece = game_state.get_board().cells[7][7]
     assert top_right_piece is not None
     assert top_right_piece.piece == Piece.rook
     assert top_right_piece.player == Player.black
+
+    middle_left_piece = game_state.get_board().cells[0][3]
+    assert middle_left_piece is None
+
+    bottom_center_piece = game_state.get_board().cells[3][0]
+    assert bottom_center_piece is not None
+    assert bottom_center_piece.piece == Piece.king
+    assert bottom_center_piece.player == Player.white
 
