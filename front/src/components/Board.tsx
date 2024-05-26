521a08b17f780642a0e61a43c86ff506d13cc9a6
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-05-26 12:31:33 +0300

    feature(front): Board component MVP

--- front/src/components/Board.tsx
+++ front/src/components/Board.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Player, Piece, CellContent } from '../model'
 import classes from './board.module.css'
 
@@ -51,4 +52,53 @@ export default function Board({
   makeMove,
 }: IBoardProps) {
 
+  const [selectedCell, setSelectedCell] = useState<{ x: number, y: number } | null>(null)
+  const onCellClick = (x: number, y: number) => {
+    if(!isCurrentPlayerTurn) {
+      // TODO: provide visual feedback (or show the whole board so that it's clear that "it's their turn")
+      return
+    }
+
+    const inCell = cells[x][y]
+    const hasClickedOwnPiece = inCell &&
+      inCell != 'is_dark' &&
+      inCell.player == currentPlayer
+
+    // unselect
+    if(selectedCell && selectedCell.x == x && selectedCell.y == y) {
+      setSelectedCell(null)
+      return
+    }
+
+    // select or reselect (another piece)
+    if(!selectedCell || hasClickedOwnPiece) {
+      if(hasClickedOwnPiece) {
+        setSelectedCell({ x, y })
+      } else {
+        // TODO: maybe provide visual feedback
+      }
+      return
+    }
+
+    makeMove(selectedCell, { x, y })
+    // TODO: provide visual feedback, maybe optimistic
+    setSelectedCell(null)
+  }
+
+  return (
+    <div className={classes.board}>
+      {// rendering goes from top to bottom, in reverse to the y axis
+      [7, 6, 5, 4, 3, 2, 1, 0].map(y =>
+        [0, 1, 2, 3, 4, 5, 6, 7].map(x =>
+          <BoardCell
+            key={`${x},${y}`}
+            x={x} y={y}
+            inside={cells[x][y]}
+            isSelected={selectedCell?.x == x && selectedCell?.y == y}
+            onClick={() => onCellClick(x, y)}
+          />
+        ).reduce((jsx, cellJSX) => <>{jsx}{cellJSX}</>)
+      ).reduce((jsx, cellJSX) => <>{jsx}{cellJSX}</>)}
+    </div>
+  )
 }
